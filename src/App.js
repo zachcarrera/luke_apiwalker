4736e6c933bbc40aab2b8ac3b6de51ec1d471a08
Zach Carrera <zachcarrera@gmail.com>
2023-03-15 12:24:48 -0700

    add in react-router-dom components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Planet } from "./components/Planet";
+import { Person } from "./components/Person";
+import { Routes, Route, Link, Navigate } from "react-router-dom";
+import { SearchForm } from "./components/SearchForm";
+import { NoneFound } from "./components/NoneFound";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <SearchForm />
+            <Routes>
+                <Route path="/" element={<div></div>} />
+                <Route path="/test" element={<Link to="/">test</Link>} />
+                <Route path="/people/:id" element={<Person />} />
+                <Route path="/planets/:id" element={<Planet />} />
+                <Route path="/not_found" element={<NoneFound />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
